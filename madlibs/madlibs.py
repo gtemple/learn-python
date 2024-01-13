2a5824313b2943706fdbdc5072fa7bad4ad3efbe
Giordano Temple <giordanotemple@gmail.com>
2024-01-13 11:03:24 -0500

    string concat

---
+++ madlibs/madlibs.py
@@ -0,0 +1,14 @@
+# youtuber = "Gio T"
+
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}, I love to {verb1}. Don't {verb2} too much or else people will think you're {famous_person}."
+
+print(madlib)
