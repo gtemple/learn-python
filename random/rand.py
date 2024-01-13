712cd02af6c0ad6bc4c4aec5f9af42f96a2c51f2
Giordano Temple <giordanotemple@gmail.com>
2024-01-13 14:07:30 -0500

    finish guessing game;

--- random/rand.py
+++ random/rand.py
@@ -11,4 +11,23 @@ def guess(x):
             print('Too high')
     print('Yup, that is the number. Exciting, right?')
 
-guess(10)
+# guess(10)
+    
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else: 
+            guess = low
+        feedback = input(f'Is {guess}, too high, too low (L), or correct (C)?').lower
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'The AI guessed {guess}, they are taking over')
+
+computer_guess(10)
